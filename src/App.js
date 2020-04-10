00eb582868e15f7ec3ea336b6882665fc3ca2ec7
Lucie Monnier <luciemonnier@MacBook-Pro-de-Lucie.local>
2020-04-10 18:10:25 +0200

    changement du texte

--- src/App.js
+++ src/App.js
@@ -33,10 +33,10 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">Bienvenue sur le Blindtest</h1>
+        <h1 className="App-title">Luluberlinux</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+        <p>From Biz dev to dev !</p>
       </div>
       <div className="App-buttons">
       </div>
